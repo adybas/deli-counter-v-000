fa9c52252c6f9e694aa11d45f08707d30d8110cc
Anna Dybas <a.m.dybas@gmail.com>
2019-09-05 20:14:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,12 +16,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  Build a method that a new customer will use when entering the deli.
-  The take_a_number method should accept two arguments, the array for the
-  current line of people (katz_deli), and a string containing the name of
-  the person joining the end of the line. The method should call out (puts)
-  the persons name along with their position in line. Top-Tip: Remember that
-  people like to count from 1, not from 0 ("zero") like computers.
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
 def now_serving
