8c056e7f7988ec7d79c4f921848b9dfa3bfdac93
Anna Dybas <a.m.dybas@gmail.com>
2019-09-05 20:12:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,6 +15,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
+  
   Build a method that a new customer will use when entering the deli.
   The take_a_number method should accept two arguments, the array for the
   current line of people (katz_deli), and a string containing the name of
