79db5bf7bc02727fafda6605d123b07ce768b24f
Anna Dybas <a.m.dybas@gmail.com>
2019-09-05 20:20:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,14 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length == 0
-    puts "The line is currently empty."
+    puts "The line is currently empty." #if the length of the array is 0, output this
   else
     current_line = []
     katz_deli.each_with_index { |customer, index| current_line << "#{index + 1}. #{customer}" }
     puts "The line is currently: #{current_line.join(" ")}"
+    #make an empty array
+    #iterate over our deli line array to extract the cutomer name, along with their position in line (index)
+    #shovel the customer name and their index number into said empty array, and use this info to make our output
   end
 end
 
