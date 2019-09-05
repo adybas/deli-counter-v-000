a3d41a6373731eac5a520fd8cfe2e9822315d1f8
Anna Dybas <a.m.dybas@gmail.com>
2019-09-05 19:58:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    current_line = []
+    katz_deli.each_with_index do |customer, index|
+      current_line << "#{index + 1}. #{customer}"
+      puts "The line is currently: #{current_line.join(" ")}"
+    end
+  end
+end
+
+def take_a_number
+end
+
+def now_serving
+end
